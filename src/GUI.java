bb78660cff4776b60871da639ac8d60b4e47da5a
zalmoujahed <zalmou2@uic.edu>
2017-11-09 15:30:41 -0600

    added a few functions

---
+++ src/GUI.java
@@ -0,0 +1,47 @@
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+
+public class GUI extends JFrame {
+
+	JMenuBar bar;
+	
+	public GUI(){
+		super("Battleship");
+		this.setLocation(500, 200);
+		
+		setMenu();
+		setGridLayout();
+		
+	}
+	//_______________________________________________________//
+	
+	public void setMenu(){
+		bar = new JMenuBar();
+		setJMenuBar(bar);
+		
+		
+		
+		
+	}
+	//_______________________________________________________//	
+	
+	public void setGridLayout(){
+		
+		
+		
+	}
+	//_______________________________________________________//
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
