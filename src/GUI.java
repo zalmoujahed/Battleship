5fdde0e131d1e6a367055504d148f625bfa566e7
zalmoujahed <zalmou2@uic.edu>
2017-11-12 20:07:58 -0600

    fixed stuff maybe

--- src/GUI.java
+++ src/GUI.java
@@ -4,43 +4,20 @@ import javax.swing.*;
 
 public class GUI extends JFrame {
 
-	Menu bar;
-<<<<<<< HEAD
 	Container container;
-	
-=======
 	//private JPanel puzzlePanel;
 	private Menu menuBar;
 
->>>>>>> fc335676e10c284b88179e1b13d3ab18e962940a
 	public GUI(){
 		super("Battleship");
 		this.setLocation(500, 200);
 		
-		setMenu();
+		InitializeGUI();
 		setGridLayout();
 		
-		InitializeGUI();
-		
 	}
 	//_______________________________________________________//
-	
-	public void setMenu(){
-		bar = new Menu();
-		setJMenuBar(bar);
-		
-	}
-	//_______________________________________________________//	
-	
-	public void setGridLayout(){
-		
-		container = getContentPane();
-		container.setLayout(new BorderLayout());
-		
-	}
 	//_______________________________________________________//
-	
-	
 	private void InitializeGUI(){
 		menuBar = new Menu();
 		//gamePanel = new SudokuGamePanel();
@@ -51,6 +28,14 @@ public class GUI extends JFrame {
 		//getContentPane().add(gamePanel, BorderLayout.CENTER);
 		setSize( 855, 800 );
 		setVisible( true );
+	}
+	//_______________________________________________________//	
+	
+	public void setGridLayout(){
+		
+		container = getContentPane();
+		container.setLayout(new BorderLayout());
+		
 	}
 	
 	
